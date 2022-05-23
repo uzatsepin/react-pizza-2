e25475a4595f251dffbe2f752ba11ff1fd496da3
Andrii Zatsepin <uzatsepin@gmail.com>
2022-05-23 13:16:50 +0300

    feat: added fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
 import Categories from './components/Categories/Categories';
 import Header from './components/Header/Header';
 import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import Sort from './components/Sort/Sort';
 import './scss/app.scss';
-import pizzas from './pizzas.json';
+// import pizzas from './pizzas.json';
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('https://628b53477886bbbb37b5ad90.mockapi.io/pizzas')
+    .then((res) => res.json())
+    .then((data) => setPizzas(data))
+  }, [])
+
   return (  
     <div className="wrapper">
       <Header/>
