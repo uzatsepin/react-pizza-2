5d06b01b83d12b83a6fe4e37ff7d56cd7595016b
Andrii Zatsepin <uzatsepin@gmail.com>
2022-05-23 18:40:12 +0300

    feat: added skeleton for pizzas

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Categories from './components/Categories/Categories';
 import Header from './components/Header/Header';
 import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import Sort from './components/Sort/Sort';
+import { Skeleton } from './components/PizzaBlock/Skeleton';
 import './scss/app.scss';
 // import pizzas from './pizzas.json';
 
 function App() {
 
   const [pizzas, setPizzas] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://628b53477886bbbb37b5ad90.mockapi.io/pizzas')
     .then((res) => res.json())
-    .then((data) => setPizzas(data))
+    .then((data) => {
+      setPizzas(data)
+      setIsLoading(false);
+    })
   }, [])
 
   return (  
@@ -28,7 +33,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((pizza) => (
+              isLoading ? [...new Array(8)].map((_, index) => <Skeleton key={index} />) : pizzas.map((pizza) => (
                 <PizzaBlock key={pizza.id} {...pizza}/>
               ))
             }
