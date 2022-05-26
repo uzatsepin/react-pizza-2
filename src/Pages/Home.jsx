704db317a3cd9cfbf02e90c8392bc243bfccd073
Andrii Zatsepin <uzatsepin@gmail.com>
2022-05-26 19:56:52 +0300

    feat: added pagination from npm package

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,16 +4,18 @@ import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
 import { Sort } from '../components/Sort/Sort';
 import { Skeleton } from '../components/PizzaBlock/Skeleton';
 import { Categories } from '../components/Categories/Categories';
+import { Pagination } from '../components/Pagination/Pagination';
 
 export const Home = ({ searchValue }) => {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярностю',
     sortProperty: 'rating',
   });
-  console.log(searchValue);
+
   useEffect(() => {
     const sortyBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
@@ -22,7 +24,7 @@ export const Home = ({ searchValue }) => {
 
     setIsLoading(true);
     fetch(
-      `https://628b53477886bbbb37b5ad90.mockapi.io/pizzas?${category}&sortBy=${sortyBy}&order=${order}${search}`,
+      `https://628b53477886bbbb37b5ad90.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortyBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((data) => {
@@ -30,7 +32,7 @@ export const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [sortType, categoryId, searchValue]);
+  }, [sortType, categoryId, searchValue, currentPage]);
 
   const pizza = pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
   const skeleton = [...new Array(8)].map((_, index) => <Skeleton key={index} />);
@@ -43,6 +45,7 @@ export const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeleton : pizza}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,17 @@
+import ReactPaginate from 'react-paginate';
+import style from './Pagination.module.scss';
+
+export const Pagination = ({ onChangePage }) => {
+  return (
+    <ReactPaginate
+      className={style.root}
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={(event) => onChangePage(event.selected + 1)}
+      pageRangeDisplayed={8}
+      pageCount={3}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+    />
+  );
+};
